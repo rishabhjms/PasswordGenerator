b71fd287f35edac85e702c1cd36893b84fa4380d
Rishabh Jain <rishabhjms99@gmail.com>
2022-12-24 20:54:08 +0530

    Minor changes in targetting

--- src/App.js
+++ src/App.js
@@ -1,45 +1,48 @@
 import './App.css';
 
-function App() {
-  const clearField=()=>{
-    const $generated_pswd = document.getElementById('generated_pswd');
+import React, { Component } from 'react'
 
+export default class App extends Component {
+  clearField = () => {
   }
-  const formula=(event)=>{
-    const $generated_pswd = document.getElementById('generated_pswd');
-    let f1 = event.target.value;
-    $generated_pswd.innerHTML = f1;
+  formula = (event) => {
+    let phrase_field = document.getElementById('phraseField')
+    let length_field = document.getElementById('lengthField');
+    let generated_pswd = document.getElementById('generated_pswd');
+    let passphrase = phrase_field.value;
+    let size = length_field.value.length;
+    
   }
-  return (
-    <div>
-      <h1 className='font-bold text-3xl px-6 py-6'>Password Generator</h1>
-      <div className='form_container grid space-y-6 px-6'>
-        <section className='grid grid-flow-col'>
-          <label htmlFor="length">Enter password length: </label>
-          <input type="text" onChange={formula}/>
-        </section>
-        <section className='grid grid-flow-col'>
-          <label htmlFor="length">Enter a passphrase: </label>
-          <input type="text" />
-        </section>
-        <section className='grid grid-flow-col'>
-          <label htmlFor="" className="s-chars">Include special characters</label>
-          <input type="checkbox" className='justify-self-start' />
-        </section>
-        <section>
-          <button className="btn" onClick={formula}>Generate</button>
-        </section>
+  render() {
+    return (
+      <div>
+        <h1 className='font-bold text-3xl px-6 py-6'>Password Generator</h1>
+        <div className='form_container grid space-y-6 px-6'>
+          <section className='grid grid-flow-col'>
+            <label htmlFor="length">Enter password length: </label>
+            <input type="text" id='lengthField' />
+          </section>
+          <section className='grid grid-flow-col'>
+            <label htmlFor="length">Enter a passphrase: </label>
+            <input type="text" id='phraseField' />
+          </section>
+          <section className='grid grid-flow-col'>
+            <label htmlFor="" className="s-chars">Include special characters</label>
+            <input type="checkbox" className='justify-self-start' />
+          </section>
+          <section>
+            <button className="btn" onClick={this.formula}>Generate</button>
+          </section>
+        </div>
+        <div className='my-12 flex flex-col space-y-4'>
+          <span className='font-bold text-3xl px-6'>Your password is </span>
+          <code className='px-6' id='generated_pswd'></code>
+          <section className='flex space-x-3'>
+            <button className="btn w-1/5 mx-6">Copy password</button>
+            <button className="btn w-1/5 mx-6" onClick={this.clearField}>Clear Field</button>
+          </section>
+        </div>
       </div>
-      <div className='my-12 flex flex-col space-y-4'>
-        <span className='font-bold text-3xl px-6'>Your password is </span>
-        <code className='px-6' id='generated_pswd'></code>
-        <section className='flex space-x-3'>
-        <button className="btn w-1/5 mx-6">Copy password</button>
-        <button className="btn w-1/5 mx-6" onClick={clearField}>Clear Field</button>
-        </section>
-      </div>
-    </div>
-  );
+    )
+  }
 }
-
-export default App;
