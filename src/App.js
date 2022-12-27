68f8496057864db8eb06b310faf68bec037f61a6
Rishabh Jain <rishabhjms99@gmail.com>
2022-12-27 13:08:57 +0530

    The password generator now works

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import './App.css';
 
 import React, { Component } from 'react'
 export default class App extends Component {
-  constructor(){
+  constructor() {
     super();
-    this.state={
+    this.state = {
       passwordFieldIsEmpty: true
     }
   }
@@ -24,41 +24,84 @@ export default class App extends Component {
     document.getElementById('generated_pswd').innerHTML = ''
     document.getElementById('phraseField').value = ''
     document.getElementById('lengthField').value = ''
+    document.getElementById('codeField').value = ''
+    document.getElementById('special_chars_tickbox').checked = false
   }
   formula = (event) => {
     //Set customised value in field
     const alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
     const field = document.getElementById('generated_pswd')
     const phrase_field = document.getElementById('phraseField')
-    const length_field = document.getElementById('lengthField')
-    for (let i = 0; i <= phrase_field.value.length; i++) {
-      for (let k = 0; k <= alphabet.length; k++) {
-        let charCodeArr = [];
-        if (phrase_field.value[i].toUpperCase() === alphabet[k]) {
-          // field.innerHTML += alphabet.indexOf(phrase_field.value[i].toUpperCase()) + 1 + Number(length_field.value) + " "
-          field.innerHTML += phrase_field.value[i].charCodeAt(0)+Number(length_field.value)+" "
+    const codeField = document.getElementById('codeField')
+    const lengthField = document.getElementById('lengthField')
+    const special_chars = document.getElementById('special_chars_tickbox')
+    if (Number(lengthField.value) < 8) {
+      alert("Sir! Please enter a bigger length.")
+    } else {
+      for (let i = 0; i < Number(lengthField.value); i++) {
+        let code = Number(codeField.value)
+        let index = Math.floor(Math.random() * alphabet.length) + code
+        if (index > 26) {
+          index -= alphabet.length
+        }
+      if (special_chars.checked) {
+        let binary = Math.random()
+        if (binary < 0.5)
+          binary = 0
+        else
+          binary = 1
+        console.log(binary)
+        const symbols = "!@#$ %^&*(){}[]=<>/:"
+        if (index % 2 === 0) {
+          if (binary === 0) {
+            field.innerHTML += alphabet[index].toLowerCase()
+          } else if (binary === 1) {
+            let f1 = Math.floor(Math.random() * symbols.length)
+            console.log("F1 is ", f1)
+            field.innerHTML += symbols[f1]
+          }
+        } else {
+          field.innerHTML += alphabet[index].toUpperCase()
+          if (binary === 0) {
+            field.innerHTML += alphabet[index].toLowerCase()
+          } else if (binary === 1) {
+            let f1 = Math.floor(Math.random() * symbols.length)
+            console.log("F1 is ", f1)
+            field.innerHTML += symbols[f1]
+          }
+        }
+      } else if (!special_chars.checked) {
+        if (index % 2 === 0) {
+          field.innerHTML += alphabet[index].toLowerCase()
+        } else {
+          field.innerHTML += alphabet[index].toUpperCase()
         }
       }
     }
   }
+}
   render() {
     return (
       <div>
         <h1 className='font-bold text-3xl px-6 py-6'>Password Generator</h1>
         <div className='form_container grid space-y-6 px-6'>
           <section className='grid grid-flow-col'>
-            <label htmlFor="length">Enter password length: </label>
-            <input type="text" id='lengthField' />
+            <label htmlFor="length">Enter passcode: </label>
+            <input type="text" id='codeField' />
           </section>
           <section className='grid grid-flow-col'>
             <label htmlFor="length">Enter a passphrase: </label>
             <input type="text" id='phraseField' />
           </section>
           <section className='grid grid-flow-col'>
-            <label htmlFor="" className="s-chars">Include special characters</label>
-            <input type="checkbox" className='justify-self-start' />
+            <label htmlFor="length">Enter length value:  </label>
+            <input type="text" id='lengthField' />
           </section>
-          <section className='space-x-4 space-y-2'>
+          <section className='grid grid-flow-col'>
+            <label htmlFor="" className="s-chars">Include special characters</label>
+            <input type="checkbox" className='justify-self-start' id='special_chars_tickbox' />
+          </section>
+          <section className='space-x-4'>
             <button className="btn" onClick={this.formula}>Generate</button>
             <button className="btn" onClick={this.clearField}>Empty fields</button>
             <button className="btn" >Copy password</button>
